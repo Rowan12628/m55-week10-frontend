1c13ab364b4097ede42917d2b70b5c053892d2d3
Rowan12628 <170511926+Rowan12628@users.noreply.github.com>
2024-07-22 14:36:41 +0100

    feature: basic header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+      <header className="headerWrap">
+        <h2>Instaclone</h2>
+        <h2>insert message here</h2>
+      </header>
+    </>
+  );
+};
+
+export default Header;
