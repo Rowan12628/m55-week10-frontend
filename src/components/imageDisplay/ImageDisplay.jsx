b24e21733a49d4d904668dd9f0c8c30b099f905c
Rowan12628 <170511926+Rowan12628@users.noreply.github.com>
2024-07-22 15:24:45 +0100

    feature: basic image display component

---
+++ src/components/imageDisplay/ImageDisplay.jsx
@@ -0,0 +1,11 @@
+import "./ImageDisplay.css";
+
+const ImageDisplay = () => {
+  return (
+    <div className="imgDisWrap">
+      <h2>Images Go Here</h2>
+    </div>
+  );
+};
+
+export default ImageDisplay;
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -3,12 +3,14 @@ import "./Layout.css";
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
 import LogOrSign from "../logOrSign/LogOrSign/LogOrSign";
+import ImageDisplay from "../imageDisplay/ImageDisplay";
 
 const Layout = () => {
   return (
     <>
       <Header />
       <LogOrSign />
+      <ImageDisplay />
       <Footer />
     </>
   );
