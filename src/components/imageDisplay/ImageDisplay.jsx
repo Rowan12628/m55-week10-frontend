96b27184738fd0eb2e1abad3aef4c10f2228410d
Rowan12628 <170511926+Rowan12628@users.noreply.github.com>
2024-07-26 16:04:12 +0100

    feature: get images from api (issue: images not rendering)

--- src/components/imageDisplay/ImageDisplay.jsx
+++ src/components/imageDisplay/ImageDisplay.jsx
@@ -1,9 +1,40 @@
 import "./ImageDisplay.css";
 
+import { useEffect, useState } from "react";
+
 const ImageDisplay = () => {
+  const [images, setImages] = useState([]);
+  const unsplash = async () => {
+    try {
+      const response = await fetch(
+        `https://api.unsplash.com/photos/?page=1&limit=3&client_id=${
+          import.meta.env.VITE_ACCESS_KEY
+        }`
+      );
+      const data = await response.json();
+      console.log(data);
+
+      setImages(data);
+
+      console.log(images);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    unsplash();
+  }, []);
+
   return (
     <div className="imgDisWrap">
-      <h2>Images Go Here</h2>
+      <h2>image</h2>
+      {images.map((image, index) => {
+        return (
+          <div key={index}>
+            <img src={image.imagesUrl} alt={image.id}></img>
+          </div>
+        );
+      })}
     </div>
   );
 };
